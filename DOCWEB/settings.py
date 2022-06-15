65633a621848e899fa24d903afb2f784727775c4
muditS96 <98409340+muditS96@users.noreply.github.com>
2022-06-15 14:17:42 +0530

    Update settings.py

--- DOCWEB/settings.py
+++ DOCWEB/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
